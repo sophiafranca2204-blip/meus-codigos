53a50e753b3d67486bd7faa640dd0b3b94e02d7f
Sophia <sophiafranca2204@gmail.com>
2026-04-28 01:44:08 -0300

    mudando

---
+++ atv008.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+
+    while(i <= 10){
+        printf("%d ao quadrado = %d\n", i, i * i);
+        i ++;
+    }
+}
---
+++ atv009.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+
+    while(i <= 5){
+        printf("%d ao cubo = %d\n", i, i * i * i);
+        i ++;
+    }
+}
